2242d6daeee74d039a22080560b5e1623a197551
tu6619 <tu6619@my.bristol.ac.uk>
2016-03-09 12:08:00 +0000

    second test

--- test/server.test.js
+++ test/server.test.js
@@ -12,3 +12,17 @@ test('server exists', function(t)  {
     t.ok(actual, 'server exists!');
     t.end();
 });
+
+test('test the content of the cookie', function(t) {
+    var options = {
+        url: '/login',
+        method: 'GET'
+    };
+
+    server.inject(options, function(response) {
+    var expected = 'someencryptedvalue1234567890';
+    var actual = response.state['user_session'];
+    t.ok(actual, expected, 'cookie content is "someencryptedvalue1234567890"');
+    t.end();
+    });
+});
