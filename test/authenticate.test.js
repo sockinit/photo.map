9eb6cd06615edabfadd711da71e6163630fdd62a
ivanmauricio <ivangh@hotmail.co.uk>
2016-03-09 17:03:13 +0000

    got token back

--- test/authenticate.test.js
+++ test/authenticate.test.js
@@ -3,18 +3,67 @@ var server = require('../lib/server.js');
 
 
 
-test('/login endpoint redirect to gh', function(t){
+test('/login endpoint redirect to gh', (t) => {
     var expected, actual;
     var options = {
         url:'/login',
         method: 'GET'
     };
 
-    server.inject(options, function(response) {
+    server.inject(options, (response) => {
+        //redirect test
         actual = response.statusCode;
         expected = 302;
-
         t.equal(actual, expected, 'server responds with 302');
+        t.end();
+    });
+});
+
+
+test('/welcome code is present', (t) => {
+    var expected, actual;
+    var options = {
+        url:'/welcome',
+        method: 'GET'
+    };
+
+    server.inject(options, (response) => {
+        //redirects to meetup
+        actual = response.statusCode;
+        expected = 302;
+        t.equal(actual, expected, 'server responds with 302');
+
+        // var redirect = response.headers.location;
+        // var redirectOptions = url.parse(redirect);
+        //
+        // actual = redirectOptions.host;
+        // expected = 'meetup.com';
+        // t.equal(actual, expected, 'redirect to meetup');
+
+        t.end();
+    });
+});
+
+test('/welcome code is present', (t) => {
+    var expected, actual;
+    var options = {
+        url:'/hello',
+        method: 'GET'
+    };
+
+    server.inject(options, (response) => {
+        //redirects to meetup
+        actual = response.statusCode;
+        expected = 302;
+        t.equal(actual, expected, 'server responds with 302');
+
+        // var redirect = response.headers.location;
+        // var redirectOptions = url.parse(redirect);
+        //
+        // actual = redirectOptions.host;
+        // expected = 'meetup.com';
+        // t.equal(actual, expected, 'redirect to meetup');
+
         t.end();
     });
 });
