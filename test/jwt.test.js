6c9045c29aabf59a1cf1e3a5e0cfda89e8176c1d
spudtrader <jmurphy.web@gmail.com>
2016-03-10 12:25:21 +0000

    tdd'd the mother

--- test/jwt.test.js
+++ test/jwt.test.js
@@ -1,3 +1,4 @@
+require('env2')('config.env');
 
 var test = require('tape');
 var jwt = require('../lib/jwt.js');
@@ -7,5 +8,14 @@ test('generateToken function returns a string, not containing the token passed t
     const encrypted = jwt.generateToken('token');
     const actual = encrypted.indexOf('token');
     const expected = -1;
-    t.equal(actual, expected , 'jwt.generateToken successfully encrypts a string')
+    t.equal(actual, expected , 'jwt.generateToken successfully encrypts a string');
+    t.end();
+});
+
+test('decodeToken returns original token back to us', (t) => {
+    const encrypted = jwt.generateToken('token');
+    const actual = jwt.decodeToken(encrypted);
+    const expected = 'token';
+    t.equal(actual, expected , 'jwt.decodeToken decrypts token');
+    t.end();
 });
