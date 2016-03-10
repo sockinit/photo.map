308ec1a89dc5b1c36859781652028930db5446b0
virginie <virginietrubiano@gmail.com>
2016-03-10 12:45:18 +0000

    add click event to the /meetup path + create coockie of currentLocation

--- public/main.js
+++ public/main.js
@@ -1,17 +1,58 @@
-var script = document.createElement('script');
 console.log('yayayaa!');
 //
-script.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key=' + secretkeys.map + '&signed_in=true&callback=initMap');
-document.body.appendChild(script);
 
 // function that creates map and shoves it on the screen
 
-console.log(secretkeys.map);
 
-var map;
+var map, currentLocation, marker;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
+      center: {lat: 51, lng: 0},
+      zoom: 10,
+      mapTypeId: google.maps.MapTypeId.HYBRID,
+      draggableCursor: 'crosshair'
     });
+
+// adds click function to store lat/long
+
+    map.addListener('click', function(el) {
+      placeMarkerAndPanTo(el.latLng, map);
+      currentLocation = {
+        lat: '' + el.latLng.lat(),
+        lng: '' + el.latLng.lng()
+      }
+
+      console.log('currentLocation', currentLocation );
+    });
+
 }
+
+// places ,arker on map and pans to given lat/long
+
+function placeMarkerAndPanTo(latLng, map) {
+  if(marker) {
+    marker.setMap(null);
+  }
+  marker = new google.maps.Marker({
+    position: latLng,
+    map: map
+  });
+
+  map.panTo(latLng);
+}
+
+document.getElementById('map').addEventListener('click', function() {
+  // var path = window.location.pathname;
+  // console.log(!(path.indexOf('/meetup') > -1));
+    if(!(window.location.pathname.indexOf('/meetup') > -1)){
+      window.location.pathname += "/meetup";
+      document.cookie="Latitude=" + currentLocation.lat;
+      document.cookie="Longitude=" + currentLocation.lng;
+
+    }
+    else if (window.location.pathname.indexOf('/meetup') > -1){
+      console.log('bonjour');
+      document.cookie="Latitude=" + currentLocation.lat;
+      document.cookie="Longitude=" + currentLocation.lng;
+    }
+});
